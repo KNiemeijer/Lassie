56f2439399ddb3539b8335b96ca0a25b3d86e66f
KNiemeijer <K.j.niemeijer@students.uu.nl>
2015-05-29 16:16:39 +0200

    Update HomeActivity

--- app/src/main/java/lassie/lassie/HomeActivity.java
+++ app/src/main/java/lassie/lassie/HomeActivity.java
@@ -1,12 +1,18 @@
 package lassie.lassie;
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
 
 public class HomeActivity extends Fragment {
+
+    View fragmentview;
 
     public HomeActivity() {
     }
@@ -14,20 +20,32 @@ public class HomeActivity extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ImageView imageview_profiel;
+        RoundImage roundedImage;
 
-        return inflater.inflate(R.layout.fragment_home_activity, container, false);
-//        ToggleButton vermist = (ToggleButton) getView().findViewById(R.id.button_vermist);
-        //      vermist.setChecked(true);
+        fragmentview = inflater.inflate(R.layout.fragment_home_activity, container, false);
+
+        // Rond profiel aanmaken
+        imageview_profiel = (ImageView) fragmentview.findViewById(R.id.imageview_profiel);
+        Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.ic_hond_voorbeeld);
+        roundedImage = new RoundImage(bm);
+        imageview_profiel.setImageDrawable(roundedImage);
+
+        ToggleButton vermist = (ToggleButton) fragmentview.findViewById(R.id.button_vermist);
+        vermist.setChecked(true);
+
+        return fragmentview;
     }
 
-    /* public void Switch(View view) {
-        ToggleButton vermist = (ToggleButton) getView().findViewById(R.id.button_vermist);
-        ToggleButton gevonden = (ToggleButton) getView().findViewById(R.id.button_gevonden);
+    public void Switch(View view) {
+        ToggleButton vermist = (ToggleButton) fragmentview.findViewById(R.id.button_vermist);
+        ToggleButton gevonden = (ToggleButton) fragmentview.findViewById(R.id.button_gevonden);
         if (view == gevonden) {
             vermist.toggle();
         }
         if (view == vermist) {
             gevonden.toggle();
         }
-    } */
+    }
 }
+
