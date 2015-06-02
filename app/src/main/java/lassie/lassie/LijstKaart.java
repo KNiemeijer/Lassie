00144af2935f7b9fa31505861fa79edd93ce3aac
SanderVB <vanbeekster@gmail.com>
2015-06-02 19:11:09 +0200

    Heleboel zooi voor de lijst-weergave, work-in-progress

--- app/src/main/java/lassie/lassie/LijstKaart.java
+++ app/src/main/java/lassie/lassie/LijstKaart.java
@@ -1,14 +1,18 @@
 package lassie.lassie;
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
 
 public class LijstKaart extends Fragment {
 
-    public LijstKaart() {
+ /*   public LijstKaart() {
     }
 
     @Override
@@ -17,5 +21,20 @@ public class LijstKaart extends Fragment {
 
         return inflater.inflate(R.layout.fragment_lijst_kaart, container, false);
 
+    }
+}*/
+
+    View fragmentview;
+    public LijstKaart() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        fragmentview = inflater.inflate(R.layout.fragment_lijst_kaart, container, false);
+
+        return fragmentview;
     }
 }
