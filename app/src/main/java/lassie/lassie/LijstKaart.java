d5ff35a3fb6ecaa2ec5ace49edb63e672245e71f
KNiemeijer <K.j.niemeijer@students.uu.nl>
2015-07-02 15:12:51 +0200

    Nog een voorlopige versie LijstKaart

--- app/src/main/java/lassie/lassie/LijstKaart.java
+++ app/src/main/java/lassie/lassie/LijstKaart.java
@@ -4,16 +4,13 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -54,14 +51,16 @@ public class LijstKaart extends Fragment {
         int gebruikersID = ((MainActivity) getActivity()).getGebruikersID();
         List<Dier> dieren = db.getAllDieren();
         int t = 1;
+        LinearLayout linearlayout = (LinearLayout) fragmentview.findViewById(R.id.lijstContent);
         for (final Dier dier : dieren) {
 
 
             // LinearLayout voor plaatje maken
             LinearLayout layout = new LinearLayout(getActivity());
-            LinearLayout.LayoutParams lp = (new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-            lp.setMargins(0, 0, 5, 0);
-            layout.setPadding(3, 3, 3, 3);
+            LinearLayout.LayoutParams lp = (new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            layout.setOrientation(LinearLayout.HORIZONTAL);
+            layout.setWeightSum(4);
+
 
             String imageviewString = "dier";
             imageviewString += t;
@@ -83,54 +82,54 @@ public class LijstKaart extends Fragment {
                 }
             });
             layout.setLayoutParams(lp);
+            lp.width = LinearLayout.LayoutParams.WRAP_CONTENT;
+            lp.weight = 1;
 
             // Plaatje invullen
             ImageView imageview = new ImageView(getActivity());
-            imageview.setMinimumWidth(50);
-            imageview.setMinimumHeight(50);
-            imageview.setMaxHeight(50);
-            imageview.setMaxWidth(50);
-            imageview.setImageDrawable(tekenen(t, dier));
+            imageview.setImageDrawable(tekenen(t));
             layout.addView(imageview);
 
             // Naam van dier invullen
             TextView textview_naam = new TextView(getActivity());
             textview_naam.setText(dier.getNaam());
-            RelativeLayout.LayoutParams params = (new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+          /*  RelativeLayout.LayoutParams params = (new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
             params.addRule(RelativeLayout.RIGHT_OF, layout.getId());
             params.addRule(RelativeLayout.ALIGN_TOP, layout.getId());
-            textview_naam.setLayoutParams(params);
+            textview_naam.setLayoutParams(params); */
+            textview_naam.setLayoutParams(lp);
             textview_naam.setTextSize(15);
-            textview_naam.setTypeface(Typeface.SANS_SERIF);
-            textview_naam.setTextColor(343434);
+            layout.addView(textview_naam);
 
             // Beschrijving van dier invullen
             TextView textview_beschrijving = new TextView(getActivity());
             textview_beschrijving.setText(dier.getEigenschappen());
-            RelativeLayout.LayoutParams params_eigenschappen = (new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+      /*      RelativeLayout.LayoutParams params_eigenschappen = (new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
             params_eigenschappen.addRule(RelativeLayout.ALIGN_BOTTOM, layout.getId());
             params_eigenschappen.addRule(RelativeLayout.END_OF, layout.getId());
             params_eigenschappen.addRule(RelativeLayout.BELOW, textview_naam.getId());
             params_eigenschappen.setMargins(0, 1, 0, 0);
-            textview_beschrijving.setLayoutParams(params_eigenschappen);
-            textview_beschrijving.setTextSize(10);
-            textview_beschrijving.setTextColor(343434);
+            textview_beschrijving.setLayoutParams(params_eigenschappen); */
+            textview_beschrijving.setLayoutParams(lp);
+            textview_beschrijving.setTextSize(15);
+            layout.addView(textview_beschrijving);
 
             // Status dier invullen
             TextView textview_status = new TextView(getActivity());
             textview_status.setText(dier.getStatus());
-            RelativeLayout.LayoutParams params_status = (new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+   /*         RelativeLayout.LayoutParams params_status = (new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
             params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
             params.addRule(RelativeLayout.ALIGN_TOP, textview_naam.getId());
-            params_status.setMargins(0, 5, 0, 0);
-            textview_status.setGravity(Gravity.RIGHT);
-            textview_status.setLayoutParams(params);
-            textview_status.setTextSize(10);
+            params_status.setMargins(0, 5, 0, 0); */
+            textview_status.setLayoutParams(lp);
+            textview_status.setTextSize(15);
             if (dier.getStatus().equals("Gevonden")) {
                 textview_status.setTextColor(getResources().getColor(R.color.green));
             } else {
                 textview_status.setTextColor(getResources().getColor(R.color.red));
             }
+            layout.addView(textview_status);
+            linearlayout.addView(layout);
 
             t++;
         }
@@ -139,15 +138,13 @@ public class LijstKaart extends Fragment {
     }
 
 
-    public Drawable tekenen(int t, Dier dier) {
+    public Drawable tekenen(int t) {
         RoundImage roundedImage;
         String imageviewString = "dier";
         imageviewString += t;
         int resourceID = getResources().getIdentifier(imageviewString,
                 "drawable", "lassie.lassie");
         Bitmap bm = BitmapFactory.decodeResource(getResources(), resourceID);
-        TextView status = new TextView(getActivity());
-        status.setText(dier.getStatus());
         roundedImage = new RoundImage(bm);
         return roundedImage;
 
